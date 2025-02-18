7be240ec7076646a8583b0cdaf2ac6c9a0de0bee
Rabab <raaa555@outlook.com>
2025-02-18 19:36:29 +0300

    finshed koltin basic null safety exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,32 @@
 fun main() {
-    println("Hello World!")
+
+
+   // var garde:Int = 8
+
+   // garde = null
+
+    var cityName:String? = "Aberdeen"
+
+
+    println(cityName?.length)
+
+    cityName = null
+
+    //elvis opertoer
+    val cityLength = cityName?.length ?: "0"
+    println(cityLength)
+
+    scoreNum(99.0)
+    scoreNum(null)
+
+    }
+
+
+// function with nullable parameter
+
+fun scoreNum( score:Double?) {
+    val result = score?.let {
+        "score is ${score}"
+    } ?: "no score"
+    println(result)
 }
